747e821ce15c11965fe89d2c859da43637350692
Chuxan12 <mishalin2003@gmail.com>
2024-12-15 23:59:20 +0400

    добавлена логика для игры, изменены схемы в бд

--- backend/app/auth/models.py
+++ backend/app/auth/models.py
@@ -1,24 +1,36 @@
-from sqlalchemy import text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.dao.database import Base, str_uniq
-
-
-class Role(Base):
-    name: Mapped[str_uniq]
-    users: Mapped[list["User"]] = relationship(back_populates="role")
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}(id={self.id}, name={self.name})"
+from sqlalchemy import ForeignKey, Integer, String, Text, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class User(Base):
-    phone_number: Mapped[str_uniq]
-    first_name: Mapped[str]
-    last_name: Mapped[str]
-    email: Mapped[str_uniq]
-    password: Mapped[str]
-    role_id: Mapped[int] = mapped_column(ForeignKey('roles.id'), default=1, server_default=text("1"))
-    role: Mapped["Role"] = relationship("Role", back_populates="users", lazy="joined")
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(50))  # Добавляем имя
+    email: Mapped[str] = mapped_column(
+        String(100), unique=True)  # Уникальная почта
+    # Аватар (может быть URL или путь к изображению)
+    avatar: Mapped[str] = mapped_column(Text)
+    password: Mapped[str] = mapped_column(String(100))  # Пароль
+    statistics: Mapped["UserStatistics"] = relationship(
+        "UserStatistics", back_populates="user", uselist=False)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(id={self.id})"
+        return f"{self.__class__.__name__}(id={self.id}, first_name={self.first_name}, email={self.email})"
+
+
+class UserStatistics(Base):
+    __tablename__ = 'user_statistics'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey(
+        'users.id'), unique=True)  # Связь один к одному
+    wins: Mapped[int] = mapped_column(Integer, default=0)  # Количество побед
+    games_played: Mapped[int] = mapped_column(
+        Integer, default=0)  # Количество игр
+    user: Mapped[User] = relationship("User", back_populates="statistics")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id}, user_id={self.user_id}, wins={self.wins}, games_played={self.games_played})"
---
+++ backend/app/game/models.py
@@ -0,0 +1,66 @@
+from sqlalchemy import ForeignKey, Integer, String, Text, UUID
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+from sqlalchemy.ext.declarative import declarative_base
+from app.auth.models import User
+
+Base = declarative_base()
+
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    player1_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    player2_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    settings_id: Mapped[int] = mapped_column(ForeignKey('settings.id'))
+    settings: Mapped["Settings"] = relationship(
+        "Settings", back_populates="games")
+    holes: Mapped[list["Hole"]] = relationship("Hole", back_populates="game")
+    kalahas: Mapped[list["Kalaha"]] = relationship(
+        "Kalaha", back_populates="game")
+
+    players: Mapped[list[User]] = relationship(
+        "User", primaryjoin="or_(User.id==player1_id, User.id==player2_id)")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id}, player1_id={self.player1_id}, player2_id={self.player2_id})"
+
+
+class Settings(Base):
+    __tablename__ = 'settings'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    stones_count: Mapped[int] = mapped_column(Integer)  # Количество камней
+    holes_count: Mapped[int] = mapped_column(Integer)  # Количество лунок
+    turn_time: Mapped[int] = mapped_column(Integer)  # Время на ход
+    games: Mapped[list["Game"]] = relationship(
+        "Game", back_populates="settings")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id}, stones_count={self.stones_count}, holes_count={self.holes_count}, turn_time={self.turn_time})"
+
+
+class Hole(Base):
+    __tablename__ = 'holes'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    number: Mapped[int] = mapped_column(Integer)  # Номер лунки
+    stones_count: Mapped[int] = mapped_column(Integer)  # Количество камней
+    game_id: Mapped[int] = mapped_column(ForeignKey('games.id'))
+    game: Mapped[Game] = relationship("Game", back_populates="holes")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id}, number={self.number}, stones_count={self.stones_count})"
+
+
+class Kalaha(Base):
+    __tablename__ = 'kalahas'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    number: Mapped[int] = mapped_column(Integer)  # Номер калаха
+    stones_count: Mapped[int] = mapped_column(Integer)  # Количество камней
+    game_id: Mapped[int] = mapped_column(ForeignKey('games.id'))
+    game: Mapped[Game] = relationship("Game", back_populates="kalahas")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id}, number={self.number}, stones_count={self.stones_count})"
