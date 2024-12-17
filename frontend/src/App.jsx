3d3fb41fe905ff8425f28b316b7f38edd444ab3d
panfilov <na2balll@yandex.ru>
2024-12-17 23:46:20 +0400

    сделаны темы, страница об игре, страница о разработчиках,

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import MainPage from "./pages/MainPage/MainPage";
@@ -7,19 +7,29 @@ import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import SettingPage from "./pages/SettingPage/SettingPage";
+import { ThemeContext } from './context/ThemeContext';
+import AboutDevelopersPage from "./pages/AboutDevelopersPage/AboutDevelopersPage";
+import AboutGamePage from "./pages/AboutGamePage/AboutGamePage"
 
 const App = () => {
+
+    const [currentTheme, setCurrentTheme] = useState('desert');
+
   return (
+     <ThemeContext.Provider value={{ currentTheme, setCurrentTheme }}>
     <Routes>
-      <Route path="/" element={<MainLayout />}>
+      <Route path="/" element={<MainLayout currentTheme={currentTheme} />}>
         <Route index element={<MainPage />} />
         <Route path="registration" element={<RegistrationPage />}/>
         <Route path="login" element={<LoginPage/>}/>
         <Route path="profile" element={<ProfilePage/>}/>
         <Route path="game_setting" element={<SettingPage/>}/>
+        <Route path="about-developers" element={<AboutDevelopersPage/>}/>
+        <Route path="about-game" element={<AboutGamePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
+    </ThemeContext.Provider>
   );
 };
 
--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "../components/Header/Header";
 import { Outlet } from "react-router-dom";
 import styles from "./MainLayout.module.css";
 
+const themes = {
+  forest: 'url(/images/forest-bg.jpg)',
+  metal: 'url(/images/metal-bg.jpg)', 
+  desert: 'url("/images/desert-bg.jpg")'
+};
+
 const MainLayout = () => {
+
+    const [currentTheme, setCurrentTheme] = useState(() => {
+    return localStorage.getItem('currentTheme') || 'desert';
+  });
+
+    useEffect(() => {
+    localStorage.setItem('currentTheme', currentTheme);
+  }, [currentTheme]);
+
   return (
-    <div className={styles.layout}>
-      <Header />
+<div 
+      className={styles.layout} 
+      style={{ backgroundImage: themes[currentTheme] }}
+    >
+      <Header setCurrentTheme={setCurrentTheme} />
       <main>
-        <Outlet /> {}
+        <Outlet />
       </main>
     </div>
   );
