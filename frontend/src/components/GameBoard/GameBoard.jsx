2dcea92c9cd47fa551c97a21e85cf8a42a368be9
panfilov <na2balll@yandex.ru>
2024-12-23 23:29:56 +0400

    minor fixes

--- frontend/src/components/GameBoard/GameBoard.jsx
+++ frontend/src/components/GameBoard/GameBoard.jsx
@@ -1,24 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import Kalah from '../Kalah/Kalah';
-import Hole from '../GameHole/GameHole';
-import styles from './GameBoard.module.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Kalah from "../Kalah/Kalah";
+import Hole from "../GameHole/GameHole";
+import styles from "./GameBoard.module.css";
+import axios from "axios";
 
 const GameBoard = () => {
   const [boardState, setBoardState] = useState({
     holes: new Map([
-      [0, 8], [1, 7], [2, 4], [3, 3], [4, 4], [5, 4], [6, 4],
-      [7, 2], [8, 12], [9, 4], [10, 4], [11, 7]
+      [0, 8],
+      [1, 7],
+      [2, 4],
+      [3, 3],
+      [4, 4],
+      [5, 4],
+      [6, 4],
+      [7, 2],
+      [8, 12],
+      [9, 4],
+      [10, 4],
+      [11, 7],
     ]),
-    kalahs: new Map([[0, 10], [1, 0]]),
-    selectedHole: null, 
+    kalahs: new Map([
+      [0, 10],
+      [1, 0],
+    ]),
+    selectedHole: null,
+    s,
   });
 
   const [showFinishButton, setShowFinishButton] = useState(false);
 
   useEffect(() => {
     async function getGameState() {
-      axios.get('api/get_game') // получаем состояние игры
+      axios
+        .get("api/get_game") // получаем состояние игры
         .then((response) => {
           const holes = new Map(Object.entries(response.data.holes));
           const kalahs = new Map(Object.entries(response.data.kalahs));
@@ -30,7 +45,7 @@ const GameBoard = () => {
           });
         })
         .catch((error) => {
-          console.log('Data fetch with error:', error);
+          console.log("Data fetch with error:", error);
         });
     }
     getGameState();
@@ -48,18 +63,26 @@ const GameBoard = () => {
     setShowFinishButton(true);
   };
 
-  const handleFinishTurn = async () => { // по нажатии завершить ход отправляем ид лунки и ждем ответ
+  const handleFinishTurn = async () => {
+    // по нажатии завершить ход отправляем ид лунки и ждем ответ
     try {
-      const response = await axios.post('/api/turn_confirm', { holeId: boardState.selectedHole });
+      const response = await axios.post("/api/turn_confirm", {
+        holeId: boardState.selectedHole,
+      });
       const data = response.data;
 
       const holes = data.holes;
       const kalahs = data.kalahs;
 
-      setBoardState((prevState) => ({ ...prevState, holes, kalahs, selectedHole: null }));
-      setShowFinishButton(false); 
+      setBoardState((prevState) => ({
+        ...prevState,
+        holes,
+        kalahs,
+        selectedHole: null,
+      }));
+      setShowFinishButton(false);
     } catch (error) {
-      console.error('Error making move:', error);
+      console.error("Error making move:", error);
     }
   };
 
@@ -75,7 +98,7 @@ const GameBoard = () => {
                 key={index}
                 seedsCount={seeds}
                 onClick={() => handleHoleClick(index)}
-                displayCount={seeds >= 8} 
+                displayCount={seeds >= 8}
                 isSelected={index === boardState.selectedHole}
               />
             ))}
@@ -88,8 +111,8 @@ const GameBoard = () => {
                 key={index}
                 seedsCount={seeds}
                 onClick={() => handleHoleClick(index)}
-                displayCount={seeds >= 8} 
-                isSelected={index === boardState.selectedHole} 
+                displayCount={seeds >= 8}
+                isSelected={index === boardState.selectedHole}
               />
             ))}
         </div>
