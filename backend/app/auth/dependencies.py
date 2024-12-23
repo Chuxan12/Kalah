e02a607017f6b9c553e9ce462e7ee2a347b4d0e8
Chuxan12 <mishalin2003@gmail.com>
2024-12-23 21:47:41 +0400

    fix auth_me

--- backend/app/auth/dependencies.py
+++ backend/app/auth/dependencies.py
@@ -7,6 +7,7 @@ from app.exceptions import TokenExpiredException, NoJwtException, NoUserIdExcept
 from app.auth.dao import UsersDAO
 from app.auth.models import User
 from app.dao.session_maker import SessionDep
+from app.auth.schemas import SUserRegister, SUserAuth, EmailModel, SUserAddDB, SUserInfo, SUserUpdate
 
 
 def get_token(request: Request):
@@ -35,6 +36,43 @@ async def get_current_user(token: str = Depends(get_token), session: AsyncSessio
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User not found')
     return user
+
+
+async def get_current_user_with_stats(
+    token: str = Depends(get_token),
+    session: AsyncSession = SessionDep
+) -> SUserInfo:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+    except JWTError:
+        raise NoJwtException
+
+    expire: str = payload.get('exp')
+    expire_time = datetime.fromtimestamp(int(expire), tz=timezone.utc)
+    if (not expire) or (expire_time < datetime.now(timezone.utc)):
+        raise TokenExpiredException
+
+    user_id: str = payload.get('sub')
+    if not user_id:
+        raise NoUserIdException
+
+    # Извлекаем пользователя вместе со статистикой
+    user = await UsersDAO.find_one_or_none_by_id(data_id=int(user_id), session=session)
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User not found')
+    
+    # Возвращаем пользователя вместе со статистикой
+    user_info = {
+        "id": user.id,
+        "first_name": user.first_name,
+        "email": user.email,
+        "avatar": user.avatar,
+        "wins": user.statistics.wins if user.statistics else 0,  # Если статистика не существует, возвращаем 0
+        "games_played": user.statistics.games_played if user.statistics else 0  # Если статистика не существует, возвращаем 0
+    }
+    
+    return user_info
 
 
 async def get_current_admin_user(current_user: User = Depends(get_current_user)):
--- backend/app/auth/schemas.py
+++ backend/app/auth/schemas.py
@@ -44,6 +44,8 @@ class SUserInfo(UserBase):
     id: int = Field(description="Идентификатор пользователя")
     first_name: Optional[str] = Field(None, max_length=50, description="Имя пользователя. Оставьте пустым, если не хотите менять.")
     avatar: Optional[str] = Field(None, description="URL или путь к изображению аватара. Оставьте пустым, если не хотите менять.")
+    wins: Optional[int] = Field(None, description="Кол-во побед.")
+    games: Optional[int] = Field(None, description="Кол-во игр.")
 
 class SUserUpdate(BaseModel):
     first_name: Optional[str] = Field(None, max_length=50, description="Имя пользователя. Оставьте пустым, если не хотите менять.")
